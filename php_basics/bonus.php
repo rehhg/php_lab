db8e628415792fceec4353195392a5966bc5485e
rehhg <rehhg@mail.ua>
2017-07-29 15:22:43 +0300

    optimized script for better result

--- php_basics/bonus.php
+++ php_basics/bonus.php
@@ -1,40 +1,28 @@
 <?php
 
-$a = "ZzZz";
-$b = "ffPFF";
+$a = null;
+$b = "";
 
 echo compareAndSum($a, $b);
 
 function compareAndSum($str1, $str2) {
-    
-    $arr1 = array();
-    $arr2 = array();
-    
-    for($i = 0; $i < strlen($str1); $i++) {
+    $arr1 = putCharsIntoArray($str1);
+    $arr2 = putCharsIntoArray($str2);
+
+    return (array_sum($arr1) == array_sum($arr2)) ? true : false;
+}
+
+function putCharsIntoArray($str) {
+    $arr = [];
+    for ($i = 0; $i < strlen($str); $i++) {
         
-        if((int)($str1[$i]) > 0){
-            $arr1 = [];
+        if ((int)($str[$i]) > 0) {
+            $arr = [];
             break;
         } else {
-            $arr1[] = ord(strtoupper($str1[$i]));
+            $arr[] = ord(strtoupper($str[$i]));
         }
         
     }
-    
-    for($i = 0; $i < strlen($str2); $i++) {
-        
-        if((int)($str2[$i]) > 0){
-            $arr2 = [];
-            break;
-        } else {
-            $arr2[] = ord(strtoupper($str2[$i]));
-        }
-        
-    }
-    
-    if(array_sum($arr1) == array_sum($arr2)) {
-        return true;
-    } else {
-        return false;
-    }
+    return $arr;
 }
