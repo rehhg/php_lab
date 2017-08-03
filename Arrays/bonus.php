aad7aa027c0bfe94095b63a67295c6f58ef4c4cd
rehhg <rehhg@mail.ua>
2017-08-03 09:06:53 +0300

    Arrays homework

---
+++ Arrays/bonus.php
@@ -0,0 +1,57 @@
+<?php
+
+// 1
+$arr1 = [20, 10, -80, 10, 10, 15, 35];
+$arr2 = [10, -80, 10, 10, 15, 35];
+
+echo findIndex($arr1) . ' ';
+echo findIndex($arr2);
+
+function findIndex($arr) {
+  $sum = 0;
+  $leftSum = 0;
+  $length = count($arr);
+
+  foreach($arr as $value) {
+      $sum += $value;
+  }
+  
+  for ($i = 0; $i < $length; $i++) {
+    $sum -= $arr[$i];
+    
+    if ($leftSum == $sum) {
+      return $i;
+    } else {
+      $leftSum += $arr[$i];
+    }
+    
+  }
+
+  return -1;
+}
+
+// 2
+echo '<br />';
+$array1 = [1, 1, 1, 2, 1, 1];
+$array2 = [0, 0, 0.55, 0, 0];
+$array3 = [3, 1, 5, 3, 7, 4, 1, 5, 7];
+
+
+print_r(getUniqueValue($array1));
+print_r(getUniqueValue($array2));
+print_r(getUniqueValue($array3));
+
+function getUniqueValue($arr) {
+    $counted = array_count_values(array_map('strval', $arr));
+    $result = array();
+
+    foreach($counted as $key => $value) {
+      if($value == 1) {
+        $result[] = $key;
+      }
+    }
+    
+    return $result;
+}
+
+
---
+++ Arrays/index.php
@@ -0,0 +1,91 @@
+<?php
+
+// 1
+$arr = [1, 3, 2, 4];
+
+foreach ($arr as $value){
+    for($i = 0; $i < $value; $i++) {
+        $newArr[] = $value;
+    }
+}
+
+print_r($newArr);
+
+// 2
+echo "<br />";
+
+$temperatures = array(33, 15, 17, 20, 23, 23, 28, 40, 21, 19, 31, 18, 30, 31, 
+    28, 23, 19, 28, 27, 30, 39, 17, 17, 20, 19, 23, 28, 30, 34, 28);
+
+temeprature($temperatures);
+
+function temeprature($arr) {
+    sort($arr);
+    $length = count($arr);
+    
+    echo 'Three lowest tepmeratures are ' . $arr[0] . ', ' . $arr[1] . ', ' . $arr[2] . '<br />';
+    echo 'Three highest tepmeratures are ' . $arr[$length-1] . ', ' . $arr[$length-2] . ', ' . $arr[$length-3] . '<br />';
+    echo 'Three middle tepmeratures are ' . $arr[($length-1)/2] . ', ' . $arr[($length-2)/2] . ', ' . $arr[$length/2] . '<br />';
+}
+
+// 3
+$books = [
+    [
+        'name' => 'Learning php, mysql & JavaScript',
+        'author' => 'Robin Nixon',
+        'price' => 30,
+        'tags' => ['php', 'javascript', 'mysql']
+    ],
+    [
+        'name' => 'Php for the Web: Visual QuickStart Guide',
+        'author' => 'Larry Ullman',
+        'price' => 25,
+        'tags' => ['php']
+    ],
+    [
+        'name' => 'pHp and MySqL for Dynamic Web Sites',
+        'author' => 'Larry Ullman',
+        'price' => 14.39,
+        'tags' => ['php', 'mysql']
+    ],
+    [
+        'name' => 'Modern PhP: New Features and Good Practices',
+        'author' => 'Josh Lockhart',
+        'price' => 24,
+        'tags' => ['php']
+    ],
+    [
+        'name' => 'JavaScript and JQuery: Interactive Front-End Web Development',
+        'author' => 'Jon Duckett',
+        'price' => 20,
+        'tags' => ['javascript', 'jquery']
+    ],
+    [
+        'name' => 'Miss Peregrine\'s Home for Peculiar Children',
+        'author' => 'Ransom Riggs',
+        'price' => 8.18
+    ]
+];
+
+function sorter($key) {
+    return function($a, $b) use($key){
+        return strnatcmp($a[$key], $b[$key]);
+    };
+}
+
+function filterTags($arr){
+    $newArr = [];
+    foreach($arr as $key => $value) {
+        if(array_key_exists("tags", $value) && in_array("php", $value["tags"])){
+            $newArr[] = $value;
+        }
+    }
+    
+    return $newArr;
+}
+
+uasort($books, sorter('price'));
+echo "<pre>";
+print_r($books); // sorted by price
+print_r(filterTags($books)); // filtered by tags which contain "php"
+echo "</pre>";
